1688eca26956f5f96f95342fdcea1d98d19d9a20
Pierre Mdawar <pierre@mdawar.dev>
2023-07-29 16:37:58 +0300

    Added Solid.js integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    solid(),
     tailwind({
       // Disable injecting a basic `base.css` import on every page.
       applyBaseStyles: false,
