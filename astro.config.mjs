a21f00d497197b3fdca5e6aba961d4d17a65914e
Pierre Mdawar <pierre@mdawar.dev>
2023-07-29 16:30:33 +0300

    Added tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind({
+      // Disable injecting a basic `base.css` import on every page.
+      applyBaseStyles: false,
+    }),
+  ],
+});
