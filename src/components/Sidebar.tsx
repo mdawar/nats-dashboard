d221b95c38b1d0f812abe985f6815be5509abf3f
Pierre Mdawar <pierre@mdawar.dev>
2023-08-07 15:21:22 +0300

    Added transition effects for the mobile menu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@ import {
   Show,
   createSignal,
   splitProps,
+  createRenderEffect,
   type ParentProps,
   type ComponentProps,
 } from 'solid-js';
@@ -19,16 +20,12 @@ import {
 } from '~/components/icons';
 // @ts-expect-error (use:clickOutside not recognized as a reference)
 import { clickOutside } from '~/lib/directives';
+import { Transition } from 'solid-transition-group';
 
 export default function Sidebar() {
-  const [display] = useDisplayMobileMenu();
-
   return (
     <>
-      <Show when={display()}>
-        <MobileSidebar />
-      </Show>
-
+      <MobileSidebar />
       <DesktopSidebar />
     </>
   );
@@ -36,63 +33,90 @@ export default function Sidebar() {
 
 // Off-canvas menu for mobile, show/hide based on off-canvas menu state.
 function MobileSidebar() {
-  const [_, setDisplay] = useDisplayMobileMenu();
+  const [display, setDisplay] = useDisplayMobileMenu();
+  // Workaround to keep the container element until the animation ends.
+  // Without this workaround the animated child elements are removed from the DOM before any animations are applied.
+  const [showContainer, setShowContainer] = createSignal(display()); // Start with the initial state of the menu.
+
+  // Track enabling the mobile menu to display the container.
+  // Hiding the container is done after the animation completes.
+  createRenderEffect(() => {
+    if (display()) {
+      setShowContainer(true);
+    }
+  });
 
   return (
-    <div class="relative z-50 lg:hidden" role="dialog" aria-modal="true">
-      {/*
-      Off-canvas menu backdrop, show/hide based on off-canvas menu state.
-
-      Entering: "transition-opacity ease-linear duration-300"
-        From: "opacity-0"
-        To: "opacity-100"
-      Leaving: "transition-opacity ease-linear duration-300"
-        From: "opacity-100"
-        To: "opacity-0"
-      */}
-      <div class="fixed inset-0 bg-gray-900/80"></div>
-
-      <div class="fixed inset-0 flex">
-        {/*
-          Off-canvas menu, show/hide based on off-canvas menu state.
-
-          Entering: "transition ease-in-out duration-300 transform"
-            From: "-translate-x-full"
-            To: "translate-x-0"
-          Leaving: "transition ease-in-out duration-300 transform"
-            From: "translate-x-0"
-            To: "-translate-x-full"
-          */}
-        {/* Note: added dark bg for consistency with the desktop menu bg color (Using bg-black/10 on top) */}
-        <div
-          class="relative mr-16 flex w-full max-w-xs flex-1 dark:bg-gray-900"
-          use:clickOutside={() => setDisplay(false)}
+    <Show when={showContainer()}>
+      <div class="relative z-50 lg:hidden" role="dialog" aria-modal="true">
+        {/* Off-canvas menu backdrop. */}
+        <Transition
+          enterActiveClass="transition-opacity ease-linear duration-300"
+          enterClass="opacity-0"
+          enterToClass="opacity-100"
+          exitActiveClass="transition-opacity ease-linear duration-300"
+          exitClass="opacity-100"
+          exitToClass="opacity-0"
+          appear={true}
+          onAfterExit={() => {
+            // Hide the menu container (Must be hidden only after the animation ends).
+            setShowContainer(false);
+          }}
         >
-          {/*
-            Close button, show/hide based on off-canvas menu state.
+          <Show when={display()}>
+            <div class="fixed inset-0 bg-gray-900/80"></div>
+          </Show>
+        </Transition>
 
-            Entering: "ease-in-out duration-300"
-              From: "opacity-0"
-              To: "opacity-100"
-            Leaving: "ease-in-out duration-300"
-              From: "opacity-100"
-              To: "opacity-0"
-            */}
-          <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-            <button
-              type="button"
-              class="-m-2.5 p-2.5"
-              onClick={() => setDisplay(false)}
-            >
-              <span class="sr-only">Close sidebar</span>
-              <CloseIcon class="h-6 w-6 text-white" />
-            </button>
-          </div>
+        <div class="fixed inset-0 flex">
+          {/* Off-canvas menu. */}
+          <Transition
+            enterActiveClass="transition ease-in-out duration-300 transform"
+            enterClass="-translate-x-full"
+            enterToClass="translate-x-0"
+            exitActiveClass="transition ease-in-out duration-300 transform"
+            exitClass="translate-x-0"
+            exitToClass="-translate-x-full"
+            appear={true}
+          >
+            <Show when={display()}>
+              <div
+                class="relative mr-16 flex w-full max-w-xs flex-1 dark:bg-gray-900"
+                use:clickOutside={() => setDisplay(false)}
+              >
+                {/*
+                  Close button, show/hide based on off-canvas menu state.
 
-          <Menu />
+                  Entering: "ease-in-out duration-300"
+                    From: "opacity-0"
+                    To: "opacity-100"
+                  Leaving: "ease-in-out duration-300"
+                    From: "opacity-100"
+                    To: "opacity-0"
+
+                  Note: Cannot use the Transition component to transition the close button.
+                  The Transition component does not support child component transitions
+                  and in its current state, the element will be removed from the DOM before
+                  any transition styles are applied.
+                */}
+                <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
+                  <button
+                    type="button"
+                    class="-m-2.5 p-2.5"
+                    onClick={() => setDisplay(false)}
+                  >
+                    <span class="sr-only">Close sidebar</span>
+                    <CloseIcon class="h-6 w-6 text-white" />
+                  </button>
+                </div>
+
+                <Menu />
+              </div>
+            </Show>
+          </Transition>
         </div>
       </div>
-    </div>
+    </Show>
   );
 }
 
