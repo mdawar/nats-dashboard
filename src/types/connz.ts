b10599b77d121378262099b9148d607a72f2a561
Pierre Mdawar <pierre@mdawar.dev>
2023-08-09 19:23:05 +0300

    Added Connz interface properties

--- src/types/connz.ts
+++ src/types/connz.ts
@@ -50,5 +50,77 @@ export type ConnzSortOpt =
   | 'reason' // Reason for a closed connection
   | 'rtt'; // Round trip time
 
-/** NATS server connections information. */
-export interface Connz {} // TODO
+/**
+ * NATS server connections information.
+ *
+ * Provides detailed information on current client connections.
+ */
+export interface Connz {
+  server_id: string;
+  /** time.Time */
+  now: string;
+  num_connections: number;
+  total: number;
+  offset: number;
+  limit: number;
+  connections: ConnInfo[];
+}
+
+/** Detailed connection information. */
+interface ConnInfo {
+  cid: number;
+  kind?: string;
+  type?: string;
+  ip: string;
+  port: number;
+  /** time.Time */
+  start: string;
+  /** time.Time */
+  last_activity: string;
+  /** time.Time */
+  stop?: string;
+  reason?: string;
+  rtt?: string;
+  uptime: string;
+  idle: string;
+  pending_bytes: number;
+  in_msgs: number;
+  out_msgs: number;
+  in_bytes: number;
+  out_bytes: number;
+  subscriptions: number;
+  name?: string;
+  lang?: string;
+  version?: string;
+  tls_version?: string;
+  tls_cipher_suite?: string;
+  tls_peer_certs?: TLSPeerCert[];
+  authorized_user?: string;
+  account?: string;
+  subscriptions_list?: string[];
+  subscriptions_list_detail?: SubDetail[];
+  jwt?: string;
+  issuer_key?: string;
+  name_tag?: string;
+  tags?: string[]; // jwt.TagList
+  /** MQTT client ID. */
+  mqtt_client?: string;
+}
+
+/** Basic information about a TLS peer certificate. */
+interface TLSPeerCert {
+  subject?: string;
+  spki_sha256?: string;
+  cert_sha256?: string;
+}
+
+/** Verbose subscription information. */
+interface SubDetail {
+  account?: string;
+  subject: string;
+  qgroup?: string;
+  sid: string;
+  msgs: number;
+  max?: number;
+  cid: number;
+}
