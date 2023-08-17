53a53c3577b7985ce05830d5001533cd410df03b
Pierre Mdawar <pierre@mdawar.dev>
2023-08-17 19:33:35 +0300

    [Button] Added compact prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,23 @@
-import { splitProps, type ComponentProps } from 'solid-js';
+import { mergeProps, splitProps, type ComponentProps } from 'solid-js';
 
-export default function Button(props: ComponentProps<'button'>) {
-  const [local, rest] = splitProps(props, ['class']);
+interface Props extends ComponentProps<'button'> {
+  compact?: boolean;
+}
+
+export default function Button(props: Props) {
+  const defaults: Partial<Props> = { compact: false };
+  const [local, rest] = splitProps(mergeProps(defaults, props), [
+    'class',
+    'compact',
+  ]);
 
   return (
     <button
       type="button"
-      class="inline-flex items-center rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+      class="inline-flex items-center rounded-md bg-cyan-600 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
       classList={{
         [local.class!]: !!local.class,
+        'px-3 py-2': !local.compact,
       }}
       {...rest}
     />
