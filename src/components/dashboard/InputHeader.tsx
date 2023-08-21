116553be39716bda82cf9a8717ebd48127163859
Pierre Mdawar <pierre@mdawar.dev>
2023-08-21 19:29:41 +0300

    Added text style for the disabled input for dark mode

--- src/components/dashboard/InputHeader.tsx
+++ src/components/dashboard/InputHeader.tsx
@@ -54,7 +54,7 @@ export default function InputHeader() {
           <ServerIcon class="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400 dark:text-gray-500" />
           <input
             id="nats-url"
-            class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 dark:bg-transparent dark:text-white dark:placeholder:text-gray-500 focus:ring-0 sm:text-sm disabled:cursor-not-allowed disabled:text-gray-500"
+            class="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 dark:bg-transparent dark:text-white dark:placeholder:text-gray-500 focus:ring-0 sm:text-sm disabled:cursor-not-allowed disabled:text-gray-500 dark:disabled:text-gray-400"
             disabled={store.active}
             placeholder="Server URL"
             type="url"
