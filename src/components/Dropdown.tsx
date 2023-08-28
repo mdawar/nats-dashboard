0c1320ce44198bf3bfa6dc392a261c4226602ed6
Pierre Mdawar <pierre@mdawar.dev>
2023-08-28 19:19:25 +0300

    [Dropdown] Added disabled property for the options

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -7,6 +7,7 @@ import { clickOutside } from '~/lib/directives';
 export interface Option<T> {
   value: T;
   label: string | number;
+  disabled?: boolean;
 }
 
 export type Options<T> = Option<T>[];
@@ -50,13 +51,14 @@ export default function Dropdown<T extends OptionType>(props: Props<T>) {
             <div class="shadow-lg rounded-md ring-1 ring-gray-900/5 bg-white dark:bg-black/10 dark:ring-white/10 dark:bg-gray-900 focus:outline-none">
               <div class="py-2 dark:bg-black/10">
                 <For each={props.options}>
-                  {({ value, label }) => (
+                  {({ value, label, disabled = false }) => (
                     <button
-                      class="block w-full text-left px-3 py-1 text-sm leading-6 text-gray-900 dark:text-white hover:bg-gray-50 dark:hover:bg-gray-800"
+                      class="block w-full text-left px-3 py-1 text-sm leading-6 text-gray-900 dark:text-white dark:disabled:text-gray-500 hover:bg-gray-50 dark:hover:bg-gray-800"
                       classList={{
                         'bg-gray-50 dark:bg-gray-800': props.active === value,
                       }}
                       role="menuitem"
+                      disabled={disabled}
                       onClick={() => {
                         props.onChange(value);
                         setShow(false);
