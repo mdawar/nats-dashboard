10c7e7faae3dea8f924d707d94b345e9b2081af4
Pierre Mdawar <pierre@mdawar.dev>
2023-09-12 17:08:14 +0300

    Use less padding and margin in InfoList

--- src/components/dashboard/InfoList.tsx
+++ src/components/dashboard/InfoList.tsx
@@ -9,11 +9,11 @@ export default function InfoList(props: Props) {
 
   return (
     <div>
-      <dl class="space-y-8 sm:space-y-0 sm:divide-y sm:divide-gray-200 dark:sm:divide-white/10">
+      <dl class="space-y-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 dark:sm:divide-white/10">
         <For each={info()}>
           {([key, value]) => (
             <Show when={value !== undefined}>
-              <div class="sm:flex sm:py-5">
+              <div class="sm:flex sm:py-4">
                 <dt class="text-sm font-medium text-gray-500 dark:text-gray-400 sm:w-40 sm:flex-shrink-0 lg:w-48">
                   {key}
                 </dt>
