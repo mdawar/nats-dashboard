d01d631717c5ad8ea3f732f9086d536d18c1317e
Pierre Mdawar <pierre@mdawar.dev>
2023-09-15 17:27:29 +0300

    Paginate the streams

--- src/components/dashboard/jetstream/StreamsList.tsx
+++ src/components/dashboard/jetstream/StreamsList.tsx
@@ -2,18 +2,28 @@ import { createMemo, createSignal, For, Show } from 'solid-js';
 
 import type { StreamDetail } from '~/types';
 import { formatStream } from '~/lib/format';
+import { paginate } from '~/lib/utils';
+import Dropdown from '~/components/Dropdown';
 import Badge, { greenIfPositive } from '~/components/Badge';
 import {
   StackedListContainer,
   Header,
   HeaderTitle,
+  HeaderControls,
+  HeaderButton,
   StackedList,
   ListItem,
 } from '~/components/dashboard/StackedList';
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from '~/components/icons';
 import SlideOver from '~/components/SlideOver';
 
 import StreamItem from './StreamItem';
 import StreamDetails from './StreamDetails';
+import { pageSizeOptions } from './options';
 
 /** Sort streams by creation time. */
 function byCreationTime(a: StreamDetail, b: StreamDetail) {
@@ -29,10 +39,36 @@ export default function StreamsList(props: Props) {
     (props.streams.slice().sort(byCreationTime) ?? []).map(formatStream)
   );
 
+  const [pageSize, setPageSize] = createSignal(10);
+  const [currentPage, setCurrentPage] = createSignal(1);
+
+  const totalPages = createMemo(() => Math.ceil(streams().length / pageSize()));
+
+  const next = () => setCurrentPage((p) => Math.min(p + 1, totalPages()));
+  const prev = () => setCurrentPage((p) => Math.max(p - 1, 1));
+
+  const isFirstPage = () => currentPage() === 1;
+  const isLastPage = () => currentPage() === totalPages();
+
+  const pageStreams = createMemo(() =>
+    paginate(streams(), pageSize(), currentPage())
+  );
+
   const [selectedName, setSelectedName] = createSignal<string>();
   const selectedStream = createMemo(() =>
-    streams().find((s) => s.name === selectedName())
+    pageStreams().find((s) => s.name === selectedName())
   );
+
+  const numStreams = () => {
+    const total = streams().length;
+    const current = pageStreams().length;
+
+    if (total > 0 && total !== current) {
+      return `${current} of ${total}`;
+    }
+
+    return total;
+  };
 
   return (
     <>
@@ -45,14 +81,52 @@ export default function StreamsList(props: Props) {
               color={greenIfPositive(streams().length)}
               class="ml-3"
             >
-              {streams().length}
+              {numStreams()}
             </Badge>
           </HeaderTitle>
+
+          <HeaderControls>
+            <div class="hidden sm:flex items-center gap-2">
+              <Show when={totalPages() > 1}>
+                <span class="text-xs leading-6 text-gray-900 dark:text-white">
+                  Page {currentPage()} of {totalPages()}
+                </span>
+              </Show>
+              <button
+                type="button"
+                class="flex flex-none items-center justify-center p-1.5 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white disabled:text-gray-400 dark:disabled:text-gray-600 disabled:cursor-default"
+                onClick={prev}
+                disabled={isFirstPage()}
+              >
+                <ChevronLeftIcon class="h-4 w-4" />
+              </button>
+              <button
+                type="button"
+                class="flex flex-none items-center justify-center p-1.5 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-white disabled:text-gray-400 dark:disabled:text-gray-600 disabled:cursor-default"
+                onClick={next}
+                disabled={isLastPage()}
+              >
+                <ChevronRightIcon class="h-4 w-4" />
+              </button>
+            </div>
+
+            <Dropdown
+              width="20"
+              options={pageSizeOptions}
+              active={pageSize()}
+              onChange={setPageSize}
+            >
+              <HeaderButton class="flex items-center gap-x-1">
+                Display
+                <ChevronDownIcon class="h-4 w-4 text-gray-500" />
+              </HeaderButton>
+            </Dropdown>
+          </HeaderControls>
         </Header>
 
         <StackedList>
           <For
-            each={streams()}
+            each={pageStreams()}
             fallback={<ListItem>No streams to display.</ListItem>}
           >
             {(stream) => (
---
+++ src/components/dashboard/jetstream/options.ts
@@ -0,0 +1,16 @@
+import type { Options } from '~/components/Dropdown';
+
+/** Streams page size options. */
+export const pageSizeOptions: Options<number> = [
+  { value: 1, label: '1' },
+  { value: 2, label: '2' },
+  { value: 5, label: '5' },
+  { value: 10, label: '10' },
+  { value: 20, label: '20' },
+  { value: 30, label: '30' },
+  { value: 40, label: '40' },
+  { value: 50, label: '50' },
+  { value: 100, label: '100' },
+  { value: 250, label: '250' },
+  { value: 500, label: '500' },
+];
