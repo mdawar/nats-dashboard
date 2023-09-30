09d00d4ce54a55a02df03053918c38ad6306e45f
Pierre Mdawar <pierre@mdawar.dev>
2023-09-30 11:06:43 +0300

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solid from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +19,6 @@ export default defineConfig({
       // Disable injecting a basic `base.css` import on every page.
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
