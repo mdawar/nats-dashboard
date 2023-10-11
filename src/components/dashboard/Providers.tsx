10b214f819a9571a60862ef9132c355872285fee
Pierre Mdawar <pierre@mdawar.dev>
2023-10-11 10:01:44 +0300

    Added solid-query devtools

--- src/components/dashboard/Providers.tsx
+++ src/components/dashboard/Providers.tsx
@@ -3,12 +3,16 @@ import type { ParentProps } from 'solid-js';
 import { StoreProvider } from '~/components/context/store';
 import { SettingsProvider } from '~/components/context/settings';
 import QueryClientProvider from '~/components/dashboard/QueryClientProvider';
+import { SolidQueryDevtools } from '@tanstack/solid-query-devtools';
 
 export default function Providers(props: ParentProps) {
   return (
     <StoreProvider>
       <SettingsProvider>
-        <QueryClientProvider>{props.children}</QueryClientProvider>
+        <QueryClientProvider>
+          {props.children}
+          <SolidQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </SettingsProvider>
     </StoreProvider>
   );
